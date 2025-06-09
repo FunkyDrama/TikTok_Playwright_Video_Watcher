6f8f83ab5e00e502892f291a69553ee826106d71
FunkyDrama <cripswhigga18@gmail.com>
2025-06-09 20:06:41 +0300

    final commit with manual captcha passing

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import time
 
 
 class TikTokViewer:
+    """Клас-переглядач відео з Tik-Tok. Є авторизація та перегляд з параметрами skip_percent"""
 
     def __init__(self) -> None:
         self.email = settings.TIKTOK_EMAIL
@@ -17,20 +18,28 @@ class TikTokViewer:
         self.page = None
 
     def _setup(self, headless: bool) -> None:
+        """Ініціалізація налаштувань браузера
+        :args bool headless: Запуск з графічним інтерфейсом або без
+        """
         playwright = sync_playwright().start()
         self.browser = playwright.chromium.launch(
+            executable_path="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
             headless=headless,
             slow_mo=1000,
             args=[
                 "--disable-blink-features=AutomationControlled",
                 "--autoplay-policy=no-user-gesture-required",
                 "--use-gl=egl",
+                "--disable-web-security",
+                "--disable-features=IsolateOrigins,site-per-process",
             ],
         )
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
 
-    def _teardown(self):
+    def _teardown(self) -> None:
+        """Скидання всіх параметрів браузера та його закриття"""
+        self.context.close()
         self.browser.close()
 
     def login(self) -> bool:
@@ -54,7 +63,16 @@ class TikTokViewer:
             self.page.keyboard.type(self.password, delay=100)
 
             self.page.click('button[type="submit"]')
-            self.page.wait_for_timeout(10000)
+            try:
+                self.page.wait_for_selector(".captcha-verify-container", timeout=5000)
+                print("[~] Пройдіть капчу для продовження...")
+                self.page.wait_for_selector(
+                    ".captcha-verify-container", state="detached", timeout=180000
+                )
+                print("[+] Капча пройдена!")
+                self.page.wait_for_timeout(5000)
+            except TimeoutError:
+                print("[~] Капча не зʼявилася, продовжуємо.")
             print("[+] Успішна авторизація")
             return True
         except TimeoutError:
@@ -64,9 +82,7 @@ class TikTokViewer:
     def search(self) -> bool:
         """Пошук відео по запиту"""
         try:
-            self.page.goto("https://www.tiktok.com", timeout=15000)
             self.page.get_by_role("searchbox").click(timeout=10000)
-
             self.page.locator("input.css-1ltjld2-InputElement.e14ntknm3").click()
             self.page.keyboard.type(self.search_query, delay=100)
             self.page.keyboard.press("Enter")
@@ -80,49 +96,58 @@ class TikTokViewer:
         return False
 
     def view_videos(self):
-        """Перегляд відео з урахуванням skip_percent"""
+        """Проглядає відео зі стрічки за результатами пошуку"""
         processed = 0
+
+        # перше відео за запитом
+        self.page.locator("div:has(a)").nth(0).click()
+
         while processed < self.max_videos:
             try:
-                self.page.locator("div:has(a)").nth(0).click()
-                self.page.evaluate("document.querySelector('video')?.play()")
-                self.page.wait_for_timeout(5000)
 
-                click_next = self.page.query_selector(
-                    "button.css-1s9jpf8-ButtonBasicButtonContainer-StyledVideoSwitch e11s2kul10"
+                # отримуємо айді та посилання на відео
+                video_url = self.page.url
+                vid_id = video_url.split("/")[-1].split("?")[0]
+
+                self.page.wait_for_function(
+                    "document.querySelector('video') && document.querySelector('video').duration > 0",
+                    timeout=10000,
                 )
 
-                # elem = items[processed]
-                # link = elem.get_attribute("href") or ""
-                # vid_id = link.split("/")[-1].split("?")[0]
+                # Отримуємо тривалість відео
+                duration = self.page.evaluate(
+                    "() => document.querySelector('video').duration"
+                )
 
-                # elem.click()
-                # self.page.wait_for_selector("video", timeout=10000)
-                # print(f"[~] Видео ID={vid_id} | {link}")
+                # Вирішуємо чи треба скіпнути відео з урахуванням skip_percent
+                if random.randint(1, 100) <= self.skip_percent:
+                    print(f"[>] Пропущено відео: {vid_id} | {video_url}")
+                    time.sleep(
+                        random.randint(1, 3)
+                    )  # імітація затримки на відео як у людини
+                else:
+                    print(
+                        f"[+] Переглянуто відео: {vid_id} | {video_url} ({duration:.1f}с)"
+                    )
+                    time.sleep(duration)
 
-                # if random.randint(1, 100) <= self.skip_percent:
-                #     print(f"[>] Пропущено видео: {vid_id}")
-                # else:
-                #     duration = self.page.evaluate(
-                #         '() => document.querySelector("video").duration'
-                #     )
-                #     print(f"[+] Просмотр: {vid_id} ({duration:.2f}s)")
-                #     time.sleep(duration)
-                #
-                # self.page.go_back()
-                self.page.wait_for_timeout(2000)
+                # Перехід до наступного відео
+                self.page.locator('button[data-e2e="arrow-right"]').click(timeout=5000)
+
                 processed += 1
+                self.page.wait_for_timeout(1000)
+
             except TimeoutError:
-                print(f"[-] Не вдалося завантажити відео={vid_id}")
+                print("[-] Timeout при завантаженні відео")
                 processed += 1
             except Exception as e:
-                print(f"[-] Помилка на відео={vid_id}: {e}")
+                print(f"[-] Помилка при перегляді відео: {e}")
                 processed += 1
 
     def run(self, headless: bool = False) -> None:
         """Функція запуску
 
-        :args bool headless: Запуск з графічним інтерфейсом або без
+        :args bool headless: Запуск з графічним інтерфейсом або без. За замовчуванням False
         """
 
         self._setup(headless)
@@ -137,6 +162,7 @@ class TikTokViewer:
 
 
 def main() -> None:
+    """Функція-ініціалізатор"""
     TikTokViewer().run()
 
 
